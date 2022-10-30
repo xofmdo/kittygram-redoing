9cf712dfbf8dffc4963993206b951803a847a952
Alexandr Kuzmich <92593750+xofmdo@users.noreply.github.com>
2022-10-30 18:59:25 +0300

    use of view - functions

--- cats/views.py
+++ cats/views.py
@@ -6,14 +6,31 @@ from .models import Cat
 from .serializers import CatSerializer
 
 
+@api_view(['GET', 'POST', 'PATCH', 'DELETE', 'PUT'])
+def cat_detail(request, pk):
+    cat = Cat.objects.get(id=pk)
+    if request.method == 'PUT' or request.method == 'PATCH':
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    serializer = CatSerializer(cat)
+    return Response(serializer.data)
+
+
 @api_view(['GET', 'POST'])
 def cat_list(request):
     if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
+        serializer = CatSerializer(data=request.data, many=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
