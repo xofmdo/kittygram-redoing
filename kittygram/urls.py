e844c71be2b98b48dfa431454316f5d5bf4312dd
Alexandr Kuzmich <92593750+xofmdo@users.noreply.github.com>
2022-10-31 16:40:13 +0300

    fixing mistakes

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -2,10 +2,11 @@ from rest_framework.routers import DefaultRouter
 from django.contrib import admin
 from django.urls import include, path
 
-from cats.views import CatViewSet
+from cats.views import CatViewSet, OwnerViewSet
 
 router = DefaultRouter()
-router.register('cats', CatViewSet, basename='cats')
+router.register('cats', CatViewSet)
+router.register('owners', OwnerViewSet)
 
 urlpatterns = [
     path('', include(router.urls), name='api-root'),
