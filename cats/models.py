66b4ea99b799f7d31ed5fe05bdcd1b2756089e8a
Alexandr Kuzmich <92593750+xofmdo@users.noreply.github.com>
2022-11-01 19:32:50 +0300

    models added - Achievement, AchievementCat and M2M communications, AchievementSerializer, CatSerializer and new methods

--- cats/models.py
+++ cats/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class Achievement(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
 class Owner(models.Model):
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
@@ -15,6 +22,16 @@ class Cat(models.Model):
     birth_year = models.IntegerField()
     owner = models.ForeignKey(
         Owner, related_name='cats', on_delete=models.CASCADE)
+    achievements = models.ManyToManyField(Achievement,
+                                          through='AchievementCat')
 
     def __str__(self):
         return self.name
+
+
+class AchievementCat(models.Model):
+    achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.achievement} {self.cat}'
